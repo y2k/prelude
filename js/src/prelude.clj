8cf1fd54fe07e07c37550086998fe54ba1a13393
y2k <itwisterlx@gmail.com>
2024-04-27 15:58:21 +0200

    update prelude

--- js/src/prelude.clj
+++ js/src/prelude.clj
@@ -1,7 +1,7 @@
 (defmacro do [& body] (concat (list 'let (vector)) body))
 
-(defmacro println [& args] (concat (list 'console/info) args))
-(defmacro eprintln [& args] (concat (list 'console/error) args))
+(defmacro println [& args] (concat (list '.info 'console) args))
+(defmacro eprintln [& args] (concat (list '.error 'console) args))
 
 (defmacro def- [k v] (list 'def ^:private k v))
 
