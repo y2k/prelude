0b7e1c15e55e15a2f624a785b07eb4f14583e07d
y2k <itwisterlx@gmail.com>
2024-05-06 19:59:22 +0200

    add declrations to prelude

--- interpreter/src/prelude.clj
+++ interpreter/src/prelude.clj
@@ -1,3 +1,14 @@
+(def * 0)
+(def + 0)
+(def = 0)
+(def concat 0)
+(def false 0)
+(def if 0)
+(def list 0)
+(def str 0)
+(def true 0)
+(def vector 0)
+
 (defmacro not= [a b] (list 'not (list '= a b)))
 
 (defn not [x] (if x false true))
