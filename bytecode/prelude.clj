f9fcbeb81a625d4c7ab15b6f87b9873928609bea
y2k <itwisterlx@gmail.com>
2024-12-04 01:31:21 +0100

    [bytecode] add prelude

---
+++ bytecode/prelude.clj
@@ -0,0 +1,25 @@
+(def - 0)
+(def * 0)
+(def / 0)
+(def + 0)
+(def = 0)
+(def concat 0)
+(def false 0)
+(def get 0)
+(def if 0)
+(def list 0)
+(def null 0)
+(def str 0)
+(def true 0)
+(def vec 0)
+(def vector 0)
+
+(defmacro defonce [name value]
+  (list 'if
+        (list 'def name)
+        'nil
+        (list 'def name value)))
+
+(defmacro not= [a b] (list 'not (list '= a b)))
+
+(defn not [x] (if x false true))
