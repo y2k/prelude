08988f09eb226f9958bf4fc250ef9999308f5cae
y2k <itwisterlx@gmail.com>
2025-01-03 22:46:30 +0100

    [java] add unescape

--- java/src/RT.java
+++ java/src/RT.java
@@ -314,4 +314,30 @@ public class RT {
   public static Runnable runnable(Supplier<Object> f) {
     return f::get;
   }
+
+  public static String unescape(Object input) {
+    var str = input.toString();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < str.length(); i++) {
+      char c = str.charAt(i);
+      if (c == '\\' && i + 1 < str.length()) {
+        char next = str.charAt(i + 1);
+        if (next == 'n') {
+          sb.append('\n');
+          i++;
+        } else if (next == '\\') {
+          sb.append('\\');
+          i++;
+        } else if (next == '\"') {
+          sb.append('\"');
+          i++;
+        } else {
+          sb.append(c);
+        }
+      } else {
+        sb.append(c);
+      }
+    }
+    return sb.toString();
+  }
 }
